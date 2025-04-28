ec1ee99921ab7ff6ec84a23820a265ec5b356544
Salman Ansari <67822188+Salman8520@users.noreply.github.com>
2025-04-28 17:05:59 +0530

    Create build.groovy

---
+++ vars/build.groovy
@@ -0,0 +1,9 @@
+def call(imageName, dockerFileLocation){
+  echo "this is building the image"
+  sh "sudo apt-get install -y docker.io"
+  sh "sudo usermod -aG docker $USER && newgrp docker"
+  sh "cd django-todo-cicd/"
+  sh "pwd"
+  sh "docker build -t "${imageName}" "${dockerFileLocation}"
+  echo "image built"
+}
